08445edf6d5cc235d5603fd85e5da4a04a5f1808
Muzaki <muzakisyahrul100@gmail.com>
2022-03-04 21:06:47 +0700

    firebase fungsi read data

--- app/src/main/java/mobile/muzaki/firebaseauthapp/MainActivity.kt
+++ app/src/main/java/mobile/muzaki/firebaseauthapp/MainActivity.kt
@@ -52,6 +52,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                         }
                     })
             R.id.showdata -> {
+                startActivity(Intent(this@MainActivity, MyListData::class.java))
             }
 
             R.id.save -> {
